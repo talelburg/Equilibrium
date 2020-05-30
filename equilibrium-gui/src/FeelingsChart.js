16236610f994a8d9a4df36da3a7126bb09260ec3
Tal Elburg <talelburg11@gmail.com>
2020-05-30 12:25:24 +0300

    Show color image on hover

--- equilibrium-gui/src/FeelingsChart.js
+++ equilibrium-gui/src/FeelingsChart.js
@@ -6,58 +6,78 @@ import {Col, Container, Row} from "react-bootstrap";
 class FeelingsChart extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {snapshots: [], feelings: []}
+        this.state = {snapshots: [], feelings: [], color_images: []}
     }
 
     componentDidMount() {
         fetch(`${window.api_server}/users/${this.props.user_id}/snapshots`)
             .then(response => response.json())
             .then(data => this.setState({snapshots: data}))
-            .then(() => this.state.snapshots.forEach(snapshot =>
+            .then(() => this.state.snapshots.forEach(snapshot => {
                     fetch(`${window.api_server}/users/${this.props.user_id}/snapshots/${snapshot}/feelings`)
                         .then(response => response.json())
                         .then(data => this.setState({feelings: this.state.feelings.concat(data)}))
-                )
+                        .then(() => fetch(`${window.api_server}/users/${this.props.user_id}/snapshots/${snapshot}/color_image/data`))
+                        .then(response => response.json())
+                        .then(data => this.setState({color_images: this.state.color_images.concat(data)}));
+                })
             );
     }
 
+    customTooltip(data) {
+        if (data.active) {
+            return (
+                <div>
+                    <img src={`data:image/jpeg;base64,${data.payload[0].payload.color_image}` } height={108} width={192}  alt=""/>
+                </div>
+            );
+        }
+        return null;
+    }
+
     render() {
-        return (
-            <Container fluid>
-                <Row>
-                    <Col><h1>Feelings Over Time</h1></Col>
-                </Row>
-                <Row>
-                    <Col><LineChart
-                        width={1800}
-                        height={650}
-                        data={
-                            this.state.feelings.map((feelings, idx) => {
-                                return {
-                                    date: moment(this.state.snapshots[idx]).format("DD/MM/YYYY HH:mm:ss:SSS"),
-                                    hunger: feelings.hunger,
-                                    thirst: feelings.thirst,
-                                    exhaustion: feelings.exhaustion,
-                                    happiness: feelings.happiness,
-                                }
-                            })
-                        }
-                        margin={{top: 5, right: 30, left: 20, bottom: 5}}
-                    >
-                        <CartesianGrid strokeDasharray="3 3"/>
-                        <XAxis dataKey="date"/>
-                        <YAxis/>
-                        <Tooltip/>
-                        <Legend/>
-                        <Line type="monotone" dataKey="hunger" stroke="#FFFF00"/>
-                        <Line type="monotone" dataKey="thirst" stroke="#FF00FF"/>
-                        <Line type="monotone" dataKey="exhaustion" stroke="#00FFFF"/>
-                        <Line type="monotone" dataKey="happiness" stroke="#000000"/>
-                        <Brush/>
-                    </LineChart></Col>
-                </Row>
-            </Container>
-        );
+        if (this.state.snapshots.length !== this.state.color_images.length) {
+            return <h1>Loading...</h1>;
+        }
+        else {
+            return (
+                <Container fluid>
+                    <Row>
+                        <Col><h1>Feelings Over Time</h1></Col>
+                    </Row>
+                    <Row>
+                        <Col><LineChart
+                            width={1800}
+                            height={650}
+                            data={
+                                this.state.feelings.map((feelings, idx) => {
+                                    return {
+                                        date: moment(this.state.snapshots[idx]).format("DD/MM/YYYY HH:mm:ss:SSS"),
+                                        hunger: feelings.hunger,
+                                        thirst: feelings.thirst,
+                                        exhaustion: feelings.exhaustion,
+                                        happiness: feelings.happiness,
+                                        color_image: this.state.color_images[idx].data
+                                    }
+                                })
+                            }
+                            margin={{top: 5, right: 30, left: 20, bottom: 5}}
+                        >
+                            <CartesianGrid strokeDasharray="3 3"/>
+                            <XAxis dataKey="date"/>
+                            <YAxis/>
+                            <Tooltip content={data => this.customTooltip(data)}/>
+                            <Legend/>
+                            <Line type="monotone" dataKey="hunger" stroke="#FFFF00"/>
+                            <Line type="monotone" dataKey="thirst" stroke="#FF00FF"/>
+                            <Line type="monotone" dataKey="exhaustion" stroke="#00FFFF"/>
+                            <Line type="monotone" dataKey="happiness" stroke="#000000"/>
+                            <Brush/>
+                        </LineChart></Col>
+                    </Row>
+                </Container>
+            );
+        }
     }
 }
 
