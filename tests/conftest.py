47ce8bcd9e36fd4e03f58e567164d88e63dc3aea
Tal Elburg <talelburg11@gmail.com>
2020-05-30 18:54:55 +0300

    Hopefully make tests pass

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,44 +1,4 @@
 import os
-import pathlib
-import socket
 import sys
 
-import mongomock
-import pytest
-
-from equilibrium.utils.sample import SampleHandler
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-
-@pytest.fixture
-def resources():
-    return pathlib.Path(__file__).absolute().parent / "resources"
-
-
-@pytest.fixture
-def parsing(resources):
-    return SampleHandler.parse(str(resources / "small.mind.gz"))
-
-
-@pytest.fixture
-def parsed(parsing):
-    return SampleHandler.data_to_full_json(".gz", {
-        "user_information": next(parsing),
-        "snapshot": next(parsing),
-    })
-
-
-@pytest.fixture(autouse=True, scope="session")
-def mongodb():
-    with mongomock.patch(servers="127.0.0.1"):
-        yield
-
-
-@pytest.fixture
-def simulate_tcp_27017():
-    s = socket.socket()
-    s.bind(("127.0.0.1", 27017))
-    s.listen()
-    yield
-    s.close()
